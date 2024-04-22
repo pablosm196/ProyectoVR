5c63a9a11387a3f9d23e86664217edac63b2e9ee
pablosm196 <pablos19@ucm.es>
2024-04-22 21:26:58 +0200

    Realidad Mixta montada

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _bullet;
+    [SerializeField]
+    private float _timeToShoot;
+    private float _elapsedTime;
+    [SerializeField]
+    private float _speed;
+
+    private void Start()
+    {
+        _elapsedTime = _timeToShoot;
+    }
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+        if(_elapsedTime >= _timeToShoot && OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            _elapsedTime = 0;
+            GameObject bullet = Instantiate(_bullet, transform.position, Quaternion.identity);
+            Rigidbody bulletRB = bullet.GetComponent<Rigidbody>();
+            bulletRB.velocity = _speed * transform.forward;
+            bulletRB.useGravity = false;
+        }
+    }
+}
